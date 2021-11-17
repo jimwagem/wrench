4c60d2de034349982c9f3fc6683cb4bfb5c7f67e
Simon Brugman <sbrugman@users.noreply.github.com>
2021-11-17 15:15:19 +0100

    Update convert_profteacher.py

--- datasets/convert_profteacher.py
+++ datasets/convert_profteacher.py
@@ -5,39 +5,33 @@ import numpy as np
 
 from wrench.dataset import load_dataset
 
-data = np.load('professor_teacher_99LFs.npz')  # where * is any file in data/
-label_matrix = data['L']  # weak source votes
-Xtrain = data['Xtrain']  # features for training on soft labels
-Xtest = data['Xtest']  # features for evaluating the model
-Ytest = data['Ytest']  # gold labels for evaluating the model
+data = np.load('professor_teacher_99LFs.npz')
+for idx in data.files:
+    print(idx, data[idx].shape)
 
-print(Xtrain.shape)
-print(Xtest.shape)
-print(Ytest.shape)
-print(label_matrix.shape)
-
-datasets_path = Path("./wrench/datasets")
-profteacher_path = (datasets_path / "profteacher")
+dataset_name = "profteacher"
+datasets_path = Path("../datasets")
+profteacher_path = (datasets_path / dataset_name)
 profteacher_path.mkdir(exist_ok=True)
 
 # Label mapping
 (profteacher_path / "label.json").write_text(json.dumps({"0": "prof", "1": "teacher"}))
 
 train = {}
-for idx, (LF, data) in enumerate(zip(label_matrix, Xtrain)):
-    train[str(idx)] = {"label": None, "weak_labels": LF.tolist(), "data": {"feature": data.tolist()}}
+for idx, (label, LF, feature) in enumerate(zip(data['Ytrain_gold'], data['L'], data['Xtrain'])):
+    train[str(idx)] = {"label": label.tolist(), "weak_labels": LF.tolist(), "data": {"feature": feature.tolist()}}
 (profteacher_path / "train.json").write_text(json.dumps(train))
 
 # 250 from test
 valid = {}
-for idx, (label, data) in enumerate(zip(Ytest[-250:], Xtest[-250:])):
-    valid[str(idx)] = {"label": label.tolist(), "weak_labels": [], "data": {"feature": data.tolist()}}
+for idx, (label, feature) in enumerate(zip(data['Ytest'][-250:], data['Xtest'][-250:])):
+    valid[str(idx)] = {"label": label.tolist(), "weak_labels": [], "data": {"feature": feature.tolist()}}
 (profteacher_path / "valid.json").write_text(json.dumps(valid))
 
 test = {}
-for idx, (label, data) in enumerate(zip(Ytest[:-250], Xtest[:-250])):
-    test[str(idx)] = {"label": label.tolist(), "weak_labels": [], "data": {"feature": data.tolist()}}
+for idx, (label, feature) in enumerate(zip(data['Ytest'][:-250], data['Xtest'][:-250])):
+    test[str(idx)] = {"label": label.tolist(), "weak_labels": [], "data": {"feature": feature.tolist()}}
 (profteacher_path / "test.json").write_text(json.dumps(test))
 
-data = load_dataset(datasets_path, dataset="profteacher")
-print(data)
+datasets = load_dataset(datasets_path, dataset=dataset_name)
+print(datasets)
