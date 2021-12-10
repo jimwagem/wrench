5e4d5f7279a506e4c214116196328feb227c6d53
Simon Brugman <sfbbrugman@gmail.com>
2021-12-10 15:59:09 +0100

    feat: add end2end ws datasets

--- datasets/convert_profteacher.py
+++ datasets/convert_profteacher.py
@@ -8,40 +8,55 @@ from wrench.dataset import load_dataset
 
 # source: https://drive.google.com/drive/folders/1v7IzA3Ab5zDEsRpLBWmJnXo5841tSOlh
 ds = [
-    ('IMDB_12LFs.npz', "imdb_12", {"0": "prof", "1": "teacher"}),
-#     ('IMDB_136LFs.npz', "imdb_136", {"0": "prof", "1": "teacher"}),
-    ('professor_teacher_99LFs.npz', "profteacher", {"0": "prof", "1": "teacher"}),
-    ('Amazon_175LFs.npz', "amazon", {"0": "prof", "1": "teacher"}),
+    ('IMDB_136LFs.npz', "imdb_136", {"0": "Negative", "1": "Positive"}, "TextDataset"),
+    ('professor_teacher_99LFs.npz', "profteacher", {"0": "prof", "1": "teacher"}, "TextDataset"),
+    ('Amazon_175LFs.npz', "amazon", {"0": "Negative", "1": "Positive"}, "TextDataset"),
 ]
 
 
-for file_name, dataset_name, labels in ds:
-    data = np.load(file_name)
-    for idx in data.files:
-        print(idx, data[idx].shape)
+if __name__ == "__main__":
+    for file_name, dataset_name, labels, dtype in ds:
+        data = np.load(file_name)
+        print(f"dataset={dataset_name}")
+        for idx in data.files:
+            print(idx, data[idx].shape)
 
-    datasets_path = Path("../datasets")
-    dataset_path = (datasets_path / dataset_name)
-    dataset_path.mkdir(exist_ok=True)
+        gold = None
+        if dataset_name == "imdb_136":
+            # IMDB136 misses Ytrain_gold
+            data2 = np.load("IMDB_12LFs.npz")
+            gold = data2["Ytrain_gold"]
 
-    # Label mapping
-    (dataset_path / "label.json").write_text(json.dumps(labels))
+            # Only label matrix L is different
+            assert (data['Ytest'] == data2['Ytest']).all()
+            assert (data['Xtrain'] == data2['Xtrain']).all()
+            assert (data['Xtest'] == data2['Xtest']).all()
 
-    train = {}
-    for idx, (label, LF, feature) in enumerate(zip(data['Ytrain_gold'], data['L'], data['Xtrain'])):
-        train[str(idx)] = {"label": label.tolist(), "weak_labels": LF.tolist(), "data": {"feature": feature.tolist()}}
-    (dataset_path / "train.json").write_text(json.dumps(train))
+        if gold is None:
+            gold = data["Ytrain_gold"]
 
-    # 250 from test
-    valid = {}
-    for idx, (label, feature) in enumerate(zip(data['Ytest'][-250:], data['Xtest'][-250:])):
-        valid[str(idx)] = {"label": label.tolist(), "weak_labels": [], "data": {"feature": feature.tolist()}}
-    (dataset_path / "valid.json").write_text(json.dumps(valid))
+        datasets_path = Path("../datasets")
+        dataset_path = (datasets_path / dataset_name)
+        dataset_path.mkdir(exist_ok=True)
 
-    test = {}
-    for idx, (label, feature) in enumerate(zip(data['Ytest'][:-250], data['Xtest'][:-250])):
-        test[str(idx)] = {"label": label.tolist(), "weak_labels": [], "data": {"feature": feature.tolist()}}
-    (dataset_path / "test.json").write_text(json.dumps(test))
+        # Label mapping
+        (dataset_path / "label.json").write_text(json.dumps(labels))
 
-    datasets = load_dataset(datasets_path, dataset=dataset_name)
-    print(datasets)
+        train = {}
+        for idx, (label, LF, feature) in enumerate(zip(gold, data['L'], data['Xtrain'])):
+            train[str(idx)] = {"label": label.tolist(), "weak_labels": LF.tolist(), "data": {"feature": feature.tolist()}}
+        (dataset_path / "train.json").write_text(json.dumps(train))
+
+        # 250 from test
+        valid = {}
+        for idx, (label, feature) in enumerate(zip(data['Ytest'][-250:], data['Xtest'][-250:])):
+            valid[str(idx)] = {"label": label.tolist(), "weak_labels": [], "data": {"feature": feature.tolist()}}
+        (dataset_path / "valid.json").write_text(json.dumps(valid))
+
+        test = {}
+        for idx, (label, feature) in enumerate(zip(data['Ytest'][:-250], data['Xtest'][:-250])):
+            test[str(idx)] = {"label": label.tolist(), "weak_labels": [], "data": {"feature": feature.tolist()}}
+        (dataset_path / "test.json").write_text(json.dumps(test))
+
+        datasets = load_dataset(datasets_path, dataset=dataset_name, dataset_type=dtype)
+        print(datasets)
