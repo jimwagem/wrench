1d33399b88ad12003afa9fae159bf76ec4382169
Simon Brugman <sfbbrugman@gmail.com>
2021-12-28 17:53:27 +0100

    feat: configurable grid run
    - run all configurations at once
    - option to run subsection for experimenting (e.g. single model)
    - multiple metrics
    - obtain weights and results from same run
    - log per run idx

--- experiments/multi_gaussian/multi_gaussian.py
+++ experiments/multi_gaussian/multi_gaussian.py
@@ -1,6 +1,10 @@
+import logging
+import os
+
 import torch
 import numpy as np
 import pandas as pd
+
 from wrench.classification import WeaSEL
 from wrench.synthetic import MultiGaussian
 from wrench.labelmodel import FlyingSquid
@@ -8,11 +12,10 @@ from wrench.utils import set_seed
 from wrench.leaderboard import ModelWrapper
 from wrench.endmodel import EndClassifierModel
 from wrench.labelmodel import Snorkel
-import csv
 
 
 def compare_weights(model, test_data, model_name, n_good_lfs=5):
-    if model_name == 'weasel':
+    if model_name.startswith('weasel'):
         w_list = [
             model.extract_weights(x)
             for x in model._init_valid_dataloader(test_data)
@@ -25,171 +28,198 @@ def compare_weights(model, test_data, model_name, n_good_lfs=5):
             fs.estimated_accuracies()
             for fs in model.label_model.model
         ]
-        weights = np.mean(w_list, axis = 0)
+        weights = np.mean(w_list, axis=0)
     else:
-        raise ValueError("model_name not understood")
+        raise ValueError(f"model_name '{model_name}' not understood")
+
     good_mean = np.mean(weights[:n_good_lfs])
     if len(weights) == n_good_lfs:
         bad_mean = np.nan
     else:
         bad_mean = np.mean(weights[n_good_lfs:])
-    return (good_mean, bad_mean)
+    total_mean = np.mean(weights)
+    return good_mean, bad_mean, total_mean
 
 
-def write_results(log_file, *args):
-    with open(log_file, 'a') as csvfile:
-        writer = csv.writer(csvfile, delimiter=',')
-        writer.writerow(args)
+def write_result_pandas(file_name, result):
+    is_first = not os.path.exists(file_name)
+    mode = 'a' if not is_first else 'w'
+    df = pd.DataFrame([result.values()], columns=result.keys())
+    df.to_csv(file_name, mode=mode, index=False, header=is_first)
 
 
-def write_results_pandas(file_name, rows, columnsm, is_first):
-    df = pd.DataFrame(rows, columns=columns)
-    if is_first:
-        df.to_csv(file_name, index=False)
+def generate_datasets(lf_type, n_lfs, n_good_lfs=5):
+    # Set type of labeling function
+    n_bad_lfs = 0
+    n_random_lfs = 0
+    n_constant_lfs = 0
+    if lf_type == "bad":
+        n_bad_lfs = n_lfs
+    elif lf_type == "random":
+        n_random_lfs = n_lfs
+    elif lf_type == "constant":
+        n_constant_lfs = n_lfs
     else:
-        df.to_csv(file_name, mode='a', index=False, header=False)
+        raise ValueError("lf_type unknown")
+
+    mg = MultiGaussian(
+        n_features=8,
+        n_visible_features=7,
+        n_good_lfs=n_good_lfs,
+        n_bad_lfs=n_bad_lfs,
+        n_random_lfs=n_random_lfs,
+        n_constant_lfs=n_constant_lfs,
+        n_class=5,
+        sample_low=-5,
+        sample_high=5
+    )
+    train_dataset = mg.generate_split(split='train', n_data=10000)
+    train_dataset.labels = None
+    valid_dataset = mg.generate_split(split='valid', n_data=1000)
+    test_dataset = mg.generate_split(split='test', n_data=1000)
+    return train_dataset, valid_dataset, test_dataset
+
+
+def instantiate_model(model_name : str):
+    if model_name.startswith('weasel'):
+        model = WeaSEL(
+            temperature=1.0,
+            dropout=0.3,
+            hidden_size=100,
+            batch_size=64,
+            real_batch_size=8,
+            test_batch_size=128,
+            n_steps=10000,
+            grad_norm=1.0,
+            backbone='MLP',
+            # backbone='BERT',
+            optimizer='AdamW',
+            optimizer_lr=5e-5,
+            optimizer_weight_decay=0.0,
+        )
+    elif model_name == 'flyingsquid':
+        model = ModelWrapper(
+            model_func=lambda: EndClassifierModel(
+                batch_size=128,
+                test_batch_size=512,
+                n_steps=1000,
+                backbone='MLP',
+                optimizer='Adam',
+                optimizer_lr=1e-2,
+                optimizer_weight_decay=0.0,
+            ),
+            label_model_func=lambda: FlyingSquid(),
+            name='2stage_MLP_flyingsquid',
+        )
+    elif model_name == 'snorkel':
+        model = ModelWrapper(
+            model_func=lambda: EndClassifierModel(
+                batch_size=128,
+                test_batch_size=512,
+                n_steps=1000,
+                backbone='MLP',
+                optimizer='Adam',
+                optimizer_lr=1e-2,
+                optimizer_weight_decay=0.0,
+            ),
+            label_model_func=lambda: Snorkel(
+                lr=0.01,
+                l2=0.0,
+                n_epochs=10,
+            ),
+            name='2stage_MLP_snorkel',
+        )
+    else:
+        raise ValueError(f"model_name '{model_name}' not understood")
+
+    return model
+
+
+def printd(d):
+    return ",".join([f"{key}='{value}'" for key, value in d.items()])
 
 
 if __name__ == '__main__':
     device = torch.device('cpu')
     seeds = 20
-    
-    model_name='weasel'
-    lf_types=['bad', 'random', 'constant']
-    log_weights=False
+
+    n_good_lfs = 5
+    models = [
+        'weasel_nofeats',
+        'weasel',
+        'snorkel',
+        'flyingsquid',
+    ]
+    lf_types = [
+        'bad',
+        'random',
+        'constant',
+    ]
+    metrics = [
+        'acc',
+        'mcc',
+    ]
 
     # Same as in Figure 2 of Weasel paper
-    lfs2 = [10, 15, 20, 25, 50, 75, 100]
-    print(lfs2)
+    task_lfs = list(range(0, 10))
     # Same as in Figure 4 of Weasel paper
-    # lfs = lfs2 + [15, 20, 25, 50, 75, 100]
-    is_first = False
-    for lf_type in lf_types:
-        print(f'testing lf type: {lf_type}')
-        rows = []
-        for n_lfs in lfs2:
-            print(f'testing {n_lfs}')
-            if log_weights:
-                good_weights = []
-                bad_weights = []
-            for i in range(seeds):
-                set_seed(i)
-                seed_results = []
+    task_lfs += [10, 15, 20, 25, 50, 75, 100]
 
-                # Set type of labeling function
-                n_bad_lfs=0
-                n_random_lfs=0
-                n_constant_lfs=0
-                if lf_type=="bad":
-                    n_bad_lfs=n_lfs
-                elif lf_type=="random":
-                    n_random_lfs=n_lfs
-                elif lf_type=="constant":
-                    n_constant_lfs=n_lfs
-                mg = MultiGaussian(n_features=8, n_visible_features=7,
-                    n_good_lfs=5,
-                    n_bad_lfs=n_bad_lfs,
-                    n_random_lfs=n_random_lfs,
-                    n_constant_lfs=n_constant_lfs,
-                    n_class=5,
-                    sample_low=-5,
-                    sample_high=5
-                )
-                train_dataset = mg.generate_split(split='train', n_data=10000)
-                train_dataset.labels = None
-                valid_dataset = mg.generate_split(split='valid', n_data=1000)
-                test_dataset = mg.generate_split(split='test', n_data=1000)
-                if model_name=='weasel':
-                    model = WeaSEL(
-                        temperature=1.0,
-                        dropout=0.3,
-                        hidden_size=100,
+    # Show INFO, WARNING and ERROR logging messages
+    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger()
 
-                        batch_size=64,
-                        real_batch_size=8,
-                        test_batch_size=128,
-                        n_steps=10000,
-                        grad_norm=1.0,
+    # Results file name (tip: change to split results per model etc.)
+    file_name = f'./results/multi_gauss_new.csv'
 
-                        backbone='MLP',
-                        # backbone='BERT',
-                        optimizer='AdamW',
-                        optimizer_lr=5e-5,
-                        optimizer_weight_decay=0.0,
-                    )
-                elif model_name=='flyingsquid':
-                    model = ModelWrapper(
-                        model_func=lambda : EndClassifierModel(
-                            batch_size=128,
-                            test_batch_size=512,
-                            n_steps=1000,
-                            backbone='MLP',
-                            optimizer='Adam',
-                            optimizer_lr=1e-2,
-                            optimizer_weight_decay=0.0,
-                        ),
-                        label_model_func=lambda: FlyingSquid(),
-                        name = '2stage_MLP_flyingsquid'
-                    )
-                elif model_name=='snorkel':
-                    model = ModelWrapper(
-                        model_func=lambda : EndClassifierModel(
-                            batch_size=128,
-                            test_batch_size=512,
-                            n_steps=1000,
-                            backbone='MLP',
-                            optimizer='Adam',
-                            optimizer_lr=1e-2,
-                            optimizer_weight_decay=0.0,
-                        ),
-                        label_model_func=lambda: Snorkel(
-                            lr=0.01,
-                            l2=0.0,
-                            n_epochs=10
-                        ),
-                        name = '2stage_MLP_snorkel'
-                    )
-                else:
-                    raise ValueError("'model_name' not understood")
-                # model.fit(
-                #     train_dataset,
-                #     valid_dataset,
-                #     10,
-                #     metric='acc',
-                #     patience=100,
-                #     device=device,
-                #     verbose=False
-                #     )
-                model.fit(
-                    train_dataset,
-                    valid_dataset,
-                    evaluation_step=10,
-                    metric='acc',
-                    patience=100,
-                    device=device,
-                    verbose=False
-                )
-                if log_weights:
-                    good_mean, bad_mean = compare_weights(model, test_data=test_dataset, model_name=model_name)
-                    good_weights.append(good_mean)
-                    bad_weights.append(bad_mean)
-                    rows.append(
-                        (lf_type, n_lfs, i, good_mean, bad_mean)
-                    )
-                else:
-                    seed_results.append(model.test(test_dataset, 'acc'))
-                
-        if log_weights:
-            columns = ['lf_type', 'n_lfs', 'seed', 'good_mean', 'bad_mean']
-            write_results_pandas(f'./results/multi_gauss_weights_{model_name}.csv', rows, columns, is_first=is_first)
-        else:
-            print(np.mean(seed_results))
-            print(np.std(seed_results))
-            write_results(f'./results/multi_gauss_{model_name}.csv', '', n_lfs, np.mean(seed_results), np.std(seed_results))
-        is_first = False
+    for model_name in models:
+        for lf_type in lf_types:
+            for n_lfs in task_lfs:
+                for i in range(seeds):
+                    run_result = {
+                        'model_name': model_name,
+                        'lf_type': lf_type,
+                        'n_good_lfs': n_good_lfs,
+                        'n_lfs': n_lfs,
+                        'run_id': i,
+                    }
+                    logger.info(f"experiment with {printd(run_result)}")
+                    set_seed(i)
 
-        # col1 = [2,3,4]
-        # col2 = ['a','b','c']
-        # df= pd.DataFrame(data=[col1, col2], columns=['col1', 'col2'])
-        # df.groupby('model_name')['accuracy'].mean()
-        # df.to_csv('hello.csv')
+                    train_dataset, valid_dataset, test_dataset = generate_datasets(
+                        lf_type, n_lfs, n_good_lfs=n_good_lfs
+                    )
+                    fit_args = {}
+                    if model_name == 'weasel_nofeats':
+                        fit_args['use_encoder_features'] = False
+
+                    logger.info("Instantiate and train model")
+                    model = instantiate_model(model_name)
+                    model.fit(
+                        train_dataset,
+                        valid_dataset,
+                        evaluation_step=10,
+                        metric='acc',
+                        patience=100,
+                        device=device,
+                        verbose=False,
+                        **fit_args,
+                    )
+
+                    logger.info("Get results from trained model")
+                    # Get weights from model
+                    (
+                        run_result['good_mean'],
+                        run_result['bad_mean'],
+                        run_result['total_mean']
+                    ) = compare_weights(
+                        model, test_data=test_dataset, model_name=model_name
+                    )
+
+                    # Obtain metrics
+                    test_results = model.test(test_dataset, metrics)
+                    for metric, score in zip(metrics, test_results):
+                        run_result[metric] = score
+
+                    logger.info('Writing results to file')
+                    write_result_pandas(file_name, run_result)
